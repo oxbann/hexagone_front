47800aaccf0b36bcf74f7649549bb7304cf3f09c
oxbann <73245016+oxbann@users.noreply.github.com>
2026-07-22 11:20:58 +0200

    Configure static Next export

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
+
+module.exports = nextConfig;
